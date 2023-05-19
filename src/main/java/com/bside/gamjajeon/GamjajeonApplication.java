9ee347c407328ecc70afb6eac169b25dd6c29eb2
seon54 <seonhyeongpark54@gmail.com>
2023-05-19 22:13:45 +0900

    feat: #1 프로젝트 세팅

---
+++ src/main/java/com/bside/gamjajeon/GamjajeonApplication.java
@@ -0,0 +1,13 @@
+package com.bside.gamjajeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamjajeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamjajeonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bside/gamjajeon/GamjajeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bside.gamjajeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamjajeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
