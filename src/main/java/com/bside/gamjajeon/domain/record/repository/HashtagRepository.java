5c00a945b36e52e3166067bd3f8da8f50cc839b9
seon54 <seonhyeongpark54@gmail.com>
2023-07-02 23:48:13 +0900

    feat: 해시태그 최근 등록한 10개 조회하도록 수정

--- src/main/java/com/bside/gamjajeon/domain/record/repository/HashtagRepository.java
+++ src/main/java/com/bside/gamjajeon/domain/record/repository/HashtagRepository.java
@@ -2,6 +2,7 @@ package com.bside.gamjajeon.domain.record.repository;
 
 import com.bside.gamjajeon.domain.record.entity.Hashtag;
 import com.bside.gamjajeon.domain.user.entity.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,7 +12,7 @@ import java.util.Optional;
 public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
     Optional<Hashtag> findByKeyword(String keyword);
 
-    @Query("select distinct h.keyword from Hashtag h join fetch RecordHashtag rh on rh.hashtag = h " +
-            "join fetch Record r on r = rh.record where r.user = :user")
-    List<String> findAllKeywordByUser(User user);
+    @Query("select distinct h.keyword, h.id from Hashtag h join fetch RecordHashtag rh on rh.hashtag = h " +
+            "join fetch Record r on r = rh.record where r.user = :user order by h.id desc")
+    List<List<String>> findAllKeywordByUser(User user, Pageable pageable);
 }
--- src/main/java/com/bside/gamjajeon/domain/record/service/HashtagService.java
+++ src/main/java/com/bside/gamjajeon/domain/record/service/HashtagService.java
@@ -4,9 +4,12 @@ import com.bside.gamjajeon.domain.record.dto.response.HashtagResponse;
 import com.bside.gamjajeon.domain.record.repository.HashtagRepository;
 import com.bside.gamjajeon.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -14,7 +17,8 @@ public class HashtagService {
     private final HashtagRepository hashtagRepository;
 
     public HashtagResponse getHashtagList(User user) {
-        List<String> keywords = hashtagRepository.findAllKeywordByUser(user);
+        List<List<String>> results = hashtagRepository.findAllKeywordByUser(user, PageRequest.of(0, 10));
+        List<String> keywords = results.stream().map(list -> list.get(0)).collect(Collectors.toList());
         return new HashtagResponse(keywords);
     }
 }
