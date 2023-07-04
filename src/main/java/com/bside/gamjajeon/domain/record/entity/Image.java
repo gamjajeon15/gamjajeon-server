067c3a21110ee3de5e1fa17d509a7fe81aad61bd
DamiSoh <sohdam93@gmail.com>
2023-07-04 12:12:33 +0900

    fix : conflict 해결 2

--- src/main/java/com/bside/gamjajeon/domain/record/entity/Image.java
+++ src/main/java/com/bside/gamjajeon/domain/record/entity/Image.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Comment;
-import org.hibernate.annotations.Fetch;
 
 import javax.persistence.*;
 
--- src/main/java/com/bside/gamjajeon/domain/record/repository/RecordRepository.java
+++ src/main/java/com/bside/gamjajeon/domain/record/repository/RecordRepository.java
@@ -1,15 +1,11 @@
 package com.bside.gamjajeon.domain.record.repository;
 
-import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import net.bytebuddy.asm.Advice;
 
 import com.bside.gamjajeon.domain.record.entity.Record;
 import com.bside.gamjajeon.domain.user.entity.User;
