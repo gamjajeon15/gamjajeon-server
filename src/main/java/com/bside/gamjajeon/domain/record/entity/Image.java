4bb462c9bc8c260832a48247de45f91ae56f8d22
DamiSoh <sohdam93@gmail.com>
2023-07-04 12:10:15 +0900

    fix : conflict 해결

--- src/main/java/com/bside/gamjajeon/domain/record/entity/Image.java
+++ src/main/java/com/bside/gamjajeon/domain/record/entity/Image.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Comment;
+import org.hibernate.annotations.Fetch;
 
 import javax.persistence.*;
 
