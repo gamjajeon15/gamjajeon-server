b5542e0080d466a41046199932c098881561417c
DamiSoh <sohdam93@gmail.com>
2023-07-04 12:10:15 +0900

    fix : conflict 해결

--- src/main/java/com/bside/gamjajeon/domain/record/entity/Image.java
+++ src/main/java/com/bside/gamjajeon/domain/record/entity/Image.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Comment;
+import org.hibernate.annotations.Fetch;
 
 import javax.persistence.*;
 
@@ -23,7 +24,7 @@ public class Image extends BaseEntity {
     @Column(name = "image_id")
     private Long id;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
     @JoinColumn(name = "record_id")
     private Record record;
 
--- src/main/java/com/bside/gamjajeon/domain/record/repository/RecordRepository.java
+++ src/main/java/com/bside/gamjajeon/domain/record/repository/RecordRepository.java
@@ -18,4 +18,5 @@ public interface RecordRepository extends JpaRepository<Record, Long> {
 	@Query("from Record r where r.user =:user and year(r.recordDate) =:year and month(r.recordDate) =:month")
 	List<Record> findAllbyRecordDate(@Param("user") User user, @Param("year") int year, @Param("month") int month,
 		Sort sort);
+	void deleteByUser(User user);
 }
