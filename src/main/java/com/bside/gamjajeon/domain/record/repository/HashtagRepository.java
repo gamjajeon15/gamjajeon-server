cd06e83079ec33d240d0443e0144641fd0fcc92b
seon54 <seonhyeongpark54@gmail.com>
2023-07-08 14:03:25 +0900

    feat: 주간 해시태그 조회 API 추가

--- src/main/java/com/bside/gamjajeon/domain/record/repository/HashtagRepository.java
+++ src/main/java/com/bside/gamjajeon/domain/record/repository/HashtagRepository.java
@@ -1,18 +1,25 @@
 package com.bside.gamjajeon.domain.record.repository;
 
-import com.bside.gamjajeon.domain.record.entity.Hashtag;
-import com.bside.gamjajeon.domain.user.entity.User;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-import java.util.Optional;
+import com.bside.gamjajeon.domain.record.entity.Hashtag;
+import com.bside.gamjajeon.domain.user.entity.User;
 
 public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
-    Optional<Hashtag> findByKeyword(String keyword);
+	Optional<Hashtag> findByKeyword(String keyword);
 
-    @Query("select distinct h.keyword, h.id from Hashtag h join fetch RecordHashtag rh on rh.hashtag = h " +
-            "join fetch Record r on r = rh.record where r.user = :user order by h.id desc")
-    List<List<String>> findAllKeywordByUser(User user, Pageable pageable);
+	@Query("select distinct h.keyword, h.id from Hashtag h join fetch RecordHashtag rh on rh.hashtag = h " +
+		"join fetch Record r on r = rh.record where r.user = :user order by h.id desc")
+	List<List<String>> findAllKeywordByUser(User user, Pageable pageable);
+
+	@Query("select h.keyword, r.moodType from Hashtag h join fetch RecordHashtag rh on rh.hashtag = h " +
+		"join fetch Record r on r = rh.record "
+		+ "where r.user = :user and r.recordDate >= DATE(:startDate) and r.recordDate <= DATE(:endDate) order by r.recordDate desc")
+	List<List<String>> findWeeklyKeywordByUserAndDate(User user, LocalDate startDate, LocalDate endDate);
 }
--- src/main/java/com/bside/gamjajeon/domain/record/service/HashtagService.java
+++ src/main/java/com/bside/gamjajeon/domain/record/service/HashtagService.java
@@ -1,24 +1,63 @@
 package com.bside.gamjajeon.domain.record.service;
 
-import com.bside.gamjajeon.domain.record.dto.response.HashtagResponse;
-import com.bside.gamjajeon.domain.record.repository.HashtagRepository;
-import com.bside.gamjajeon.domain.user.entity.User;
-import lombok.RequiredArgsConstructor;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import com.bside.gamjajeon.domain.record.dto.response.HashtagResponse;
+import com.bside.gamjajeon.domain.record.dto.response.WeeklyHashtagResponse;
+import com.bside.gamjajeon.domain.record.repository.HashtagRepository;
+import com.bside.gamjajeon.domain.record.repository.RecordRepository;
+import com.bside.gamjajeon.domain.user.entity.User;
+
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
 public class HashtagService {
-    private final HashtagRepository hashtagRepository;
+	private final HashtagRepository hashtagRepository;
+	private final RecordRepository recordRepository;
 
-    public HashtagResponse getHashtagList(User user) {
-        List<List<String>> results = hashtagRepository.findAllKeywordByUser(user, PageRequest.of(0, 10));
-        List<String> keywords = results.stream().map(list -> list.get(0)).collect(Collectors.toList());
-        return new HashtagResponse(keywords);
-    }
+	public HashtagResponse getHashtagList(User user) {
+		List<List<String>> results = hashtagRepository.findAllKeywordByUser(user, PageRequest.of(0, 10));
+		List<String> keywords = results.stream().map(list -> list.get(0)).collect(Collectors.toList());
+		return new HashtagResponse(keywords);
+	}
+
+	public WeeklyHashtagResponse getWeeklyHashtagList(User user, LocalDate startDate, LocalDate endDate) {
+		WeeklyHashtagResponse weeklyHashtagResponse = new WeeklyHashtagResponse();
+		findHashtags(user, startDate, endDate, weeklyHashtagResponse);
+		findRecordCount(user, startDate, endDate, weeklyHashtagResponse);
+		return weeklyHashtagResponse;
+	}
+
+	private void findHashtags(User user, LocalDate startDate, LocalDate endDate,
+		WeeklyHashtagResponse weeklyHashtagResponse) {
+		List<List<String>> result = hashtagRepository.findWeeklyKeywordByUserAndDate(user, startDate, endDate);
+
+		for (List<String> list : result) {
+			String hashtag = list.get(0);
+			String moodType = list.get(1);
+
+			if (moodType.equals("0"))
+				weeklyHashtagResponse.getPotatoTags().add(hashtag);
+			else
+				weeklyHashtagResponse.getSweetPotatoTags().add(hashtag);
+		}
+	}
+
+	private void findRecordCount(User user, LocalDate startDate, LocalDate endDate,
+		WeeklyHashtagResponse weeklyHashtagResponse) {
+		List<List<Integer>> moodTypeCount = recordRepository.findMoodTypeCountByUserAndDate(user, startDate.toString(), endDate.toString());
+
+		if (!moodTypeCount.isEmpty()) {
+			weeklyHashtagResponse.setTotalPotato(moodTypeCount.get(0).get(0));
+			if (moodTypeCount.get(0).size() > 1) {
+				weeklyHashtagResponse.setTotalSweetPotato(moodTypeCount.get(0).get(1));
+			}
+		}
+	}
 }
