7ff070b74243f8fdb979e3bba03457074d1d63e5
DamiSoh <sohdam93@gmail.com>
2023-07-09 17:53:19 +0900

    fix : git rebase

--- src/main/java/com/bside/gamjajeon/domain/record/repository/RecordRepository.java
+++ src/main/java/com/bside/gamjajeon/domain/record/repository/RecordRepository.java
@@ -1,32 +1,20 @@
 package com.bside.gamjajeon.domain.record.repository;
 
 import java.time.LocalDate;
-
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import com.bside.gamjajeon.domain.record.dto.response.MoodResponse;
+import net.bytebuddy.asm.Advice;
+
 import com.bside.gamjajeon.domain.record.entity.Record;
 import com.bside.gamjajeon.domain.user.entity.User;
 
 public interface RecordRepository extends JpaRepository<Record, Long> {
-
-	void deleteByUser(User user);
-
-	@Query("SELECT COUNT(CASE r.moodType WHEN  0 THEN 1 END) AS potato, "
-		+ "COUNT(CASE WHEN r.moodType = 1 THEN 1 END) AS sweetPotato "
-		+ "FROM Record r WHERE r.user = :user AND YEAR(r.createdAt) = :year")
-	MoodResponse findMoodTypeByUserAndYear(User user, Integer year);
-
-	@Query(
-		"SELECT COUNT(CASE r.moodType WHEN 0 THEN 1 END) AS potato, COUNT(CASE WHEN r.moodType = 1 THEN 1 END) AS sweetPotato "
-			+ "FROM Record r WHERE r.user = :user AND r.recordDate >= DATE(:startDate) and r.recordDate <= DATE(:endDate)")
-	MoodResponse findMoodTypeCountByUserAndDate(User user, LocalDate startDate, LocalDate endDate);
-
 	@Query("from Record r where r.user =:user and year(r.recordDate) =:year and month(r.recordDate) =:month")
 	List<Record> findAllbyRecordDate(@Param("user") User user, @Param("year") int year, @Param("month") int month,
 		Sort sort);
